36a54cc04dcb88967630c47318dfc9f46475bb4f
secondskoll <michael.park@canonical.com>
2024-09-30 11:02:36 +1000

    Ch 2 finished

--- asteroids/main.py
+++ asteroids/main.py
@@ -1,16 +1,38 @@
 import pygame
 from constants import *
+from player import Player
+
+
 
 def main():
     print("Starting asteroids!")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
+
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(000000)
+
+        screen.fill("black")
+
+        for thing in updateable:
+            thing.update(dt)
+        for thing in drawable:
+            thing.draw(screen)
+
         pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
